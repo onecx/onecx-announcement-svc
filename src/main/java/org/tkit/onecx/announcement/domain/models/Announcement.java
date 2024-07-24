ab76540b1b76e0af17b8c6f2f55e56b8f904a8a6
Jannik Steenken <jannik.steenken@capgemini.com>
2024-07-24 10:52:17 +0200

    fix: rename uppercase indices

--- src/main/java/org/tkit/onecx/announcement/domain/models/Announcement.java
+++ src/main/java/org/tkit/onecx/announcement/domain/models/Announcement.java
@@ -12,10 +12,10 @@ import lombok.*;
 @Getter
 @Setter
 @Entity
-@Table(name = "announcement", indexes = {
-        @Index(name = "START_STATUS_IDX", columnList = "startDate, status, TENANT_ID"),
+@Table(name = "ANNOUNCEMENT", indexes = {
+        @Index(name = "START_STATUS_IDX", columnList = "STARTDATE, STATUS, TENANT_ID"),
         @Index(name = "PRODUCT_IDX", columnList = "PRODUCT_NAME, TENANT_ID"),
-        @Index(name = "WORKSPACE_IDX", columnList = "workspaceName, TENANT_ID")
+        @Index(name = "WORKSPACE_IDX", columnList = "WORKSPACENAME, TENANT_ID")
 })
 @SuppressWarnings("java:S2160")
 public class Announcement extends TraceableEntity {
